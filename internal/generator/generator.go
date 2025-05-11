15a5d35b8883539d5d19900d84edec24633c53a0
Raphaël Vigée <raphaelvigee@users.noreply.github.com>
2025-05-11 11:17:23 +0100

    Update generator.go
    Signed-off-by: Raphaël Vigée <raphaelvigee@users.noreply.github.com>

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -222,7 +222,7 @@ func (g *codegen) genMapField(gf *protogen.GeneratedFile, field *protogen.Field)
 	if field.Desc.MapKey().Kind() == protoreflect.BoolKind {
 		for _, k := range []bool{false, true} {
 			gf.P("if v, ok := ", fieldName, "[", k, "]; ok {")
-			g.genSingularField(gf, field.Desc.MapKey(), k)
+			gf.P(writeFn, appendBoolFn, "(nil, ", k, "))")
 			g.genSingularField(gf, field.Desc.MapValue(), "v")
 			gf.P("}")
 		}
